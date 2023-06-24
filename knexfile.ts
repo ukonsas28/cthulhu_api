2a2d1d7ad45ba9f25e9a6d40f338bbfd54588c5f
ukonsas <ukonsas40@gmail.com>
2023-06-24 11:32:25 +0300

    initial commit

---
+++ knexfile.ts
@@ -0,0 +1,57 @@
+import type { Knex } from 'knex';
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'developer',
+      password: 'developer',
+      database: 'cthulhu',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
+
+module.exports = config;
---
+++ migrations/20230618150246_users.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+const tableName = 'users';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(tableName, (t) => {
+    t.increments('id');
+    t.text('name');
+    t.text('surname');
+    t.timestamp('created_at').defaultTo(knex.fn.now());
+    t.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(tableName);
+}

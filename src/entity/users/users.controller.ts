83e417691eeb106fa20030352312a14877546a70
ukonsas <ukonsas40@gmail.com>
2023-06-27 00:14:37 +0300

    chore: add swagger + update docker configuration

--- src/entity/users/users.controller.ts
+++ src/entity/users/users.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, HttpCode, Param, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('/')
+  @ApiOperation({ summary: 'Получить список юзеров' })
+  @Get()
   getUsersList() {
     return this.usersService.getUsersList();
   }
 
   @HttpCode(200)
-  @Get('/:userId')
+  @Get(':userId')
   getUserById(@Param('userId', ParseIntPipe) userId: number) {
     return this.usersService.getUserById(userId);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cthulhy API')
+    .setDescription('Cthulhy API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
 }
 bootstrap();
